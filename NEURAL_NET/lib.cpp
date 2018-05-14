e97bbd4107f9ed6604df2095a9f1772c79b0a35e
RubikXie <xieshiyong1995@163.com>
2018-05-14 22:02:18 +0800

    this is Neural NET and  necessary train set

---
+++ NEURAL_NET/lib.cpp
@@ -0,0 +1,9 @@
+//
+//  lib.cpp
+//  NEURAL_NET
+//
+//  Created by xieshiyong on 2018/5/1.
+//  Copyright © 2018年 xieshiyong. All rights reserved.
+//
+
+#include "lib.hpp"
---
+++ NEURAL_NET/lib.hpp
@@ -0,0 +1,14 @@
+//
+//  lib.hpp
+//  NEURAL_NET
+//
+//  Created by xieshiyong on 2018/5/1.
+//  Copyright © 2018年 xieshiyong. All rights reserved.
+//
+
+#ifndef lib_hpp
+#define lib_hpp
+
+#include <stdio.h>
+
+#endif /* lib_hpp */
